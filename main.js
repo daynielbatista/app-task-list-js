770bc2189a78b0dacbabc51bf9ab7689acde1364
Dayniel <daynielbatista@gmail.com>
2020-04-29 00:26:35 +0100

    Second video finished

--- main.js
+++ main.js
@@ -6,10 +6,14 @@
 
     // Funciones
     var agregarTarea = function(){
+        
         var tarea = tareaInput.value,
+            num = lista.children.length +1,
+            numMasTarea = num +" "+ tarea,
             nuevaTarea = document.createElement("li"),
             enlace = document.createElement("a"),
-            contenido = document.createTextNode(tarea);
+            contenido = document.createTextNode(numMasTarea);
+            
 
         if (tarea ==="") {
             tareaInput.setAttribute("placeholder", "Agrega una tarea valida");
@@ -20,21 +24,30 @@
         //Agregamos el contenido al enlace
         enlace.appendChild(contenido);
         //Le establecemos un atributo href
-        enlace.setAttribute("href", "#");
-        //Agregamos el enlace(a) a la nueva tarea (li)
-        nuevaTarea.appendChild(enlace);
+        enlace.setAttribute("href", "#"); 
+        //Agregamos el enlace(a) a la nueva tarea (li)        
+        nuevaTarea.appendChild(enlace);        
         //Agregamos la nueva tarea a la lista
         lista.appendChild(nuevaTarea);
 
         tareaInput.value = "";
+
+        for (var i = 0; i <= lista.children.length -1; i++) {
+            lista.children[i].addEventListener("click", function () {
+                this.parentNode.removeChild(this);
+            });
+            
+        }
     };
 
     var comprobarInput = function(){
-        
+        tareaInput.className = "";
+        tareaInput.setAttribute("placeholder", "Agrega tu tarea");
     };
 
     var eliminarTarea = function(){
-        
+       this.parentNode.removeChild(this);
+
     };
 
     // Eventos
@@ -42,12 +55,13 @@
     //Agregar Tarea
     btnNuevaTarea.addEventListener("click", agregarTarea);
 
-    //Comprobar Imput
+    //Comprobar Input
     tareaInput.addEventListener("click", comprobarInput);
-
+    tareaInput.className = "";
+    tareaInput.setAttribute("placeholder", "Agrega tu tarea");
+    
     //Borrando elementos de la lista
-    for (var i = 0; i < lista.children.length-1; i++) {
-        lista.children[i].addEventListener("click", eliminarTarea);        
+    for (var i = 0; i <= lista.children.length -1; i++) {
+    lista.children[i].addEventListener("click", eliminarTarea); 
     }
-
-}());
+}());  
